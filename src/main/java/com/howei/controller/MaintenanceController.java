77bc3fd847c421d02c85d62716f5d2b9e13eff96
wangjie <1121382295@qq.com>
2021-07-07 13:12:40 +0800

    修改:日志登记-检修日志登记,有选择项目部时工作安排A没为空的异常

--- src/main/java/com/howei/controller/MaintenanceController.java
+++ src/main/java/com/howei/controller/MaintenanceController.java
@@ -4,18 +4,16 @@ package com.howei.controller;
 import com.howei.pojo.*;
 import com.howei.service.MaintainService;
 import com.howei.service.UserService;
-import com.howei.util.DateFormat;
-import com.howei.util.Type;
+import com.howei.util.*;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.howei.service.MaintenanceService;
-
-import com.howei.util.JsonResult;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -281,62 +279,68 @@ public class MaintenanceController {
      */
 
     @RequestMapping("getDefectList")
-    public JsonResult getDefectList(Integer departmentId, String date) {
-        if (departmentId != null && !date.equals("")) {
-            List<Map<String, Object>> mapList = new ArrayList<>();
-            Map map = new HashMap();
+    public JsonResult getDefectList(
+            @RequestParam(required = false) Integer departmentId,
+            @RequestParam(required = false) String date) {
 
-            map.put("departmentId", departmentId);
-            map.put("date", date);
-            List<Defect> defectLis = maintenanceService.getDefectList(map);
-            List<MaintainRecord> maintainRecordList = maintenanceService.getMaintainRecordList(map);
-            if (defectLis != null && defectLis.size() > 0) {
-                for (Defect defect : defectLis) {
-                    map = new HashMap();
-                    map.put("number", defect.getNumber());
-                    map.put("abs", defect.getAbs());
-                    String[] arr = defect.getEmpIds().split(",");
-                    String empIdsName = "";
-                    for (String str : arr) {
-                        Users users = userService.findByEmpId(str);
-                        if (users != null) {
-                            empIdsName += users.getUserName() + "、";
-                        }
-                    }
-                    empIdsName = empIdsName != null ? empIdsName.substring(0, empIdsName.length() - 1) : null;
-                    map.put("id", defect.getId());
-                    map.put("empIdsName", empIdsName);
-                    map.put("realExecuteTime", defect.getRealExecuteTime());
-                    map.put("confirmer1Time", defect.getConfirmer1Time());
-                    map.put("type", 0);
-                    mapList.add(map);
-                }
-            }
+        List<Map<String, Object>> mapList = new ArrayList<>();
+        Map map = new HashMap();
+        Subject subject = SecurityUtils.getSubject();
+        Users loginUser = (Users) subject.getPrincipal();
 
-            if (maintainRecordList != null && maintainRecordList.size() > 0) {
-                for (MaintainRecord maintainRecord : maintainRecordList) {
-                    map = new HashMap();
-                    map.put("number", maintainRecord.getMaintainRecordNo());
-                    map.put("abs", maintainRecord.getSystemName() + maintainRecord.getEquipmentName() + maintainRecord.getUnitName());
-                    String[] arr = maintainRecord.getEmployeeId().split(",");
-                    String empIdsName = "";
-                    for (String str : arr) {
-                        Users users = userService.findByEmpId(str);
-                        if (users != null) {
-                            empIdsName += users.getUserName() + "、";
-                        }
-                    }
-                    empIdsName = empIdsName != null ? empIdsName.substring(0, empIdsName.length() - 1) : null;
-                    map.put("empIdsName", empIdsName);
-                    map.put("realExecuteTime", maintainRecord.getWorkingHour());
-                    map.put("confirmer1Time", maintainRecord.getEndTime());
-                    map.put("type", 1);
-                    mapList.add(map);
-                }
-            }
-
-            return new JsonResult(mapList);
+        if (departmentId == null || departmentId == 0) {
+            departmentId = loginUser.getDepartmentId();
         }
-        return new JsonResult(null);
+        map.put("departmentId", departmentId);
+        if (date != null && !"".equals(date)) {
+            map.put("date", date);
+        }
+        List<Defect> defectLis = maintenanceService.getDefectList(map);
+        List<MaintainRecord> maintainRecordList = maintenanceService.getMaintainRecordList(map);
+        if (defectLis != null && defectLis.size() > 0) {
+            for (Defect defect : defectLis) {
+                map = new HashMap();
+                map.put("number", defect.getNumber());
+                map.put("abs", defect.getAbs());
+                String[] arr = defect.getEmpIds().split(",");
+                String empIdsName = "";
+                for (String str : arr) {
+                    Users users = userService.findByEmpId(str);
+                    if (users != null) {
+                        empIdsName += users.getUserName() + "、";
+                    }
+                }
+                empIdsName = empIdsName != null ? empIdsName.substring(0, empIdsName.length() - 1) : null;
+                map.put("id", defect.getId());
+                map.put("empIdsName", empIdsName);
+                map.put("realExecuteTime", defect.getRealExecuteTime());
+                map.put("confirmer1Time", defect.getConfirmer1Time());
+                map.put("type", 0);
+                mapList.add(map);
+            }
+        }
+
+        if (maintainRecordList != null && maintainRecordList.size() > 0) {
+            for (MaintainRecord maintainRecord : maintainRecordList) {
+                map = new HashMap();
+                map.put("number", maintainRecord.getMaintainRecordNo());
+                map.put("abs", maintainRecord.getSystemName() + maintainRecord.getEquipmentName() + maintainRecord.getUnitName());
+                String[] arr = maintainRecord.getEmployeeId().split(",");
+                String empIdsName = "";
+                for (String str : arr) {
+                    Users users = userService.findByEmpId(str);
+                    if (users != null) {
+                        empIdsName += users.getUserName() + "、";
+                    }
+                }
+                empIdsName = empIdsName != null ? empIdsName.substring(0, empIdsName.length() - 1) : null;
+                map.put("empIdsName", empIdsName);
+                map.put("realExecuteTime", maintainRecord.getWorkingHour());
+                map.put("confirmer1Time", maintainRecord.getEndTime());
+                map.put("type", 1);
+                mapList.add(map);
+            }
+        }
+        return new JsonResult( mapList);
     }
 }
